9f7bcd25f6fc756eaea2dba2feb79d0fe84e13b4
azamat <a_salimzhan@kbtu.kz>
2026-02-16 18:05:27 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ practice3/classes/class_variables.py
@@ -0,0 +1,11 @@
+class Student:
+    school = "W3Schools Academy"
+
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("Aigul")
+s2 = Student("Arslan")
+
+print(s1.school)
+print(s2.school)
